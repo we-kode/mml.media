76401396b079b13395b8a7d7053f28b33284a737
stummk <stummk@users.noreply.github.com>
2022-07-17 19:09:27 +0200

    Update Startup.cs

--- Media.API/Startup.cs
+++ Media.API/Startup.cs
@@ -142,7 +142,7 @@ public class Startup
   // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
   public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
   {
-    //app.UseApiKeyValidation();
+    app.UseApiKeyValidation();
     // Configure the HTTP request pipeline.
     if (env.IsDevelopment())
     {
