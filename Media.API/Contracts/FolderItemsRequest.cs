2c24dd549cc52c4949e0f81a90e57b195e7fc56c
stummk <stummk@users.noreply.github.com>
2024-02-17 17:49:11 +0100

    Locked records are not deleted when folders are deleted

---
+++ Media.API/Contracts/FolderItemsRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Media.API.Contracts
+{
+  public class FolderItemsRequest
+  {
+    /// <summary>
+    /// Items to be requested.
+    /// </summary>
+    public List<RecordFolder> Items { get; set; } = new List<RecordFolder>();
+  }
+}
---
+++ Media.DBContext/Migrations/20240217161434_Added_Locked_Record_Column.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Media.DBContext.Migrations
+{
+    public partial class Added_Locked_Record_Column : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "locked",
+                schema: "public",
+                table: "records",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "locked",
+                schema: "public",
+                table: "records");
+        }
+    }
+}
