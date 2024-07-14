65078ea93d4eafdc1db3c4902a55b690c83c265b
Dennis Stumm <ds695sas@gmail.com>
2024-07-14 19:37:54 +0200

    Added cover columns

---
+++ Media.DBContext/Migrations/20240714173117_AddedCover.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Media.DBContext.Migrations
+{
+  public partial class AddedCover : Migration
+  {
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.AddColumn<string>(
+          name: "cover",
+          schema: "public",
+          table: "albums",
+          type: "text",
+          nullable: true);
+
+      migrationBuilder.AddColumn<string>(
+          name: "cover",
+          schema: "public",
+          table: "artists",
+          type: "text",
+          nullable: true);
+
+      migrationBuilder.AddColumn<string>(
+          name: "cover",
+          schema: "public",
+          table: "genres",
+          type: "text",
+          nullable: true);
+
+      migrationBuilder.AddColumn<string>(
+          name: "cover",
+          schema: "public",
+          table: "livestreams",
+          type: "text",
+          nullable: true);
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.DropColumn(
+          name: "cover",
+          schema: "public",
+          table: "albums");
+
+      migrationBuilder.AddColumn<string>(
+          name: "cover",
+          schema: "public",
+          table: "artists");
+
+      migrationBuilder.AddColumn<string>(
+          name: "cover",
+          schema: "public",
+          table: "genres");
+
+      migrationBuilder.AddColumn<string>(
+          name: "cover",
+          schema: "public",
+          table: "livestreams");
+    }
+  }
+}
