e712e57faf8c6d68854376df742dcb62e81deaf3
stummk <stummk@users.noreply.github.com>
2026-03-04 14:12:34 +0100

    #83 Update cover oindexing to file instead of base64
    Covers are now indexed as files. Old covers will be migrated. Add endpoint to load covers for records.

---
+++ Media.Application/Contracts/IO/ICoverLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+
+namespace Media.Application.Contracts.IO;
+
+/// <summary>
+/// Handles cover save load operations
+/// </summary>
+public interface ICoverLoader
+{
+  /// <summary>
+  /// Saves the given cover data to the file system. The file name is generated based on the content of the cover. This is used to avoid storing duplicate covers and to be able to easily retrieve the cover file based on the content.
+  /// </summary>
+  /// <param name="data">The data to save</param>
+  /// <param name="fileName">The filename on disk</param>
+  /// <returns></returns>
+  Task Save(byte[] data, string fileName);
+
+  /// <summary>
+  /// Loads the cover data from the file system based on the given file name. The file name is generated based on the content of the cover. This is used to avoid storing duplicate covers and to be able to easily retrieve the cover file based on the content.
+  /// </summary>
+  /// <param name="fileName">File to load.</param>
+  /// <returns>The byte data of the file or null if file not exists.</returns>
+  Task<byte[]?> Load(string fileName);
+}
---
+++ Media.Application/Extensions/CoverExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Media.Application.Extensions;
+
+public static class CoverExtensions
+{
+  /// <summary>
+  /// Creates a file name based on the content of the cover. This is used to avoid storing duplicate covers and to be able to easily retrieve the cover file based on the content.
+  /// </summary>
+  /// <param name="coverData">The cover content</param>
+  /// <returns>The new filename of the cover.</returns>
+  public static string ToFileName(this byte[] coverData)
+  {
+    var hash = System.Security.Cryptography.SHA256.HashData(coverData);
+    string fileName = System.Buffers.Text.Base64Url.EncodeToString(hash).Substring(0, 16);
+    return $"{fileName}.jpg";
+  }
+}
